492db63a9f30eecc0b445e69c30aa844a4a2b7a4
pedroemanuel42 <99288851+pedroemanuel42@users.noreply.github.com>
2022-03-26 13:47:19 -0300

    Catching forms datas

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Estou dizendo para o express usar o EJS como View Engine
 app.set('view engine', 'ejs');
 // Uso de arquivos estáticos (css, imgs, etc...)
 app.use(express.static('public'));
 
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Rotas
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -15,7 +21,9 @@ app.get('/ask', (req, res) => {
 });
 
 app.post('/asksave', (req, res) => {
-    res.send('Formulário respondido!')
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(`Formulário respondido! Título: ${title} Descrição: ${description}.`)
 });
 
 app.listen(8080, () => {
